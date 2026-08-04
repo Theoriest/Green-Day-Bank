9cfd91631ae5077a268dee27cdfdab5d24806793
Brian kimanzi <migelbrian3@gmail.com>
2026-08-04 22:37:26 +0300

    Started on the Deposit money method

--- src/main/java/com/theoriest/greendaybank/BankingApp.java
+++ src/main/java/com/theoriest/greendaybank/BankingApp.java
@@ -7,12 +7,15 @@ public class BankingApp {
 
     }
     // 1. Show balance method
-    // 2. Deposit money method
+    // 2. Deposit money method - Brian
+
+
     // 3. Withdraw money method
     // 4. Send money to a person
-    // 5. Invest in funds
+    // 5. Invest in funds - Brian
+
     // 6. Transfer between accounts
     // 7. Withdraw all investments
-    // 8. Logout
+    // 8. Logout - Brian
     // 9. Exit
 }
