1b740e82dea34b4a801eccdd15da3324f35530f6
Brian kimanzi <migelbrian3@gmail.com>
2026-08-04 22:31:43 +0300

    new code update

--- src/main/java/com/theoriest/greendaybank/BankingApp.java
+++ src/main/java/com/theoriest/greendaybank/BankingApp.java
@@ -1,4 +1,18 @@
 package com.theoriest.greendaybank;
 
 public class BankingApp {
+
+    // main class file
+    public static void main(String[] args) {
+
+    }
+    // 1. Show balance method
+    // 2. Deposit money method
+    // 3. Withdraw money method
+    // 4. Send money to a person
+    // 5. Invest in funds
+    // 6. Transfer between accounts
+    // 7. Withdraw all investments
+    // 8. Logout
+    // 9. Exit
 }
